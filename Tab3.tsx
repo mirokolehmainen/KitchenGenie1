b1a796ce5b46d9871535ae0dfe98a9763d4c8e2f
mirokolehmainen <91200254+mirokolehmainen@users.noreply.github.com>
2023-05-16 19:01:33 +0300

    Add files via upload

--- Tab3.tsx
+++ Tab3.tsx
@@ -1,8 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem,IonInput, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import { FormEvent } from 'react';
 
 const Tab3: React.FC = () => {
+  function NewInfo(event: FormEvent<HTMLFormElement>): void {
+    throw new Error('Function not implemented.');
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +21,37 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Profile </IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+      </IonContent>
+
+
+      <IonContent>
+    <form onSubmit={NewInfo}>
+  <IonList>
+    <IonItem>
+      <IonInput label="Username" placeholder="Enter text"></IonInput>
+    </IonItem>
+  
+    <IonItem>
+      <IonInput label="Password" type="password" value="password"></IonInput>
+    </IonItem>
+  
+    <IonItem>
+      <IonInput label="New password" type="password" value="password"></IonInput>
+    </IonItem>
+  
+    <IonItem>
+      <IonInput label="Email" type="email" placeholder="email@domain.com"></IonInput>
+    </IonItem>
+
+  
+    <IonButton color={'success'} type="submit" expand="block" style={{ color: '#fff', backgroundColor: '#3880ff', margin: '10px 0', borderRadius: '25px' }}>Update</IonButton>
+  </IonList>
+</form>
+
+
+
+
+
       </IonContent>
     </IonPage>
   );
