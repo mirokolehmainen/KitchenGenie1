172055d0d2d6885e1037fae6e45bcc41c6b2a557
mirokolehmainen <91200254+mirokolehmainen@users.noreply.github.com>
2023-05-16 18:58:04 +0300

    Add files via upload

--- Home.tsx
+++ Home.tsx
@@ -1,64 +1,60 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonSearchbar, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
-import Search from './Search';
-import { useId } from 'react';
+import {
+  IonButtons,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 
-const Tab1: React.FC = () => {
-  function setCurrentFood(arg0: string) {
-    throw new Error('Function not implemented.');
-  }
 
+const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home </IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+    <>
+      <Menu />
+
+      <IonPage id="main-content">
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Or choose from ready lists </IonTitle>
+          <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>
+            Kitchen Genie</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Home" />
-      <IonSearchbar placeholder="Search for ingredients"></IonSearchbar>
-      </IonContent>
-      
-      <IonContent>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonHeader>
 
-      <IonList>
-      <IonItem>
-        <IonSelect aria-label="Fruit" placeholder="Select all fruits that apply" multiple={true}>
-          <IonSelectOption value="apples">Apples</IonSelectOption>
-          <IonSelectOption value="oranges">Oranges</IonSelectOption>
-          <IonSelectOption value="bananas">Bananas</IonSelectOption>
-        </IonSelect>
-      </IonItem>
-    </IonList>
-    <IonList>
-      <IonItem>
-        <IonSelect aria-label="Fruit" placeholder="Select all fruits that apply" multiple={true}>
-          <IonSelectOption value="apples">Apples</IonSelectOption>
-          <IonSelectOption value="oranges">Oranges</IonSelectOption>
-          <IonSelectOption value="bananas">Bananas</IonSelectOption>
-        </IonSelect>
-      </IonItem>
-    </IonList>
-    <IonList>
-      <IonItem>
-        <IonSelect aria-label="Fruit" placeholder="Select all fruits that apply" multiple={true}>
-          <IonSelectOption value="apples">Apples</IonSelectOption>
-          <IonSelectOption value="oranges">Oranges</IonSelectOption>
-          <IonSelectOption value="bananas">Bananas</IonSelectOption>
-        </IonSelect>
-      </IonItem>
-    </IonList>
-      </IonContent>
-    </IonPage>
+            <IonToolbar>
+              <IonTitle size="large">Welcome to use our app!</IonTitle>
+            </IonToolbar>
+
+            <IonItem routerLink="/recipes">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle>View recipes</IonCardTitle>
+                </IonCardHeader>
+              </IonCard>
+            </IonItem>
+
+          </IonHeader>
+        </IonContent>
+      </IonPage>
+    </>
   );
 };
 
-export default Tab1;
+export default Home;
